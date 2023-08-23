048a557e5fcb7cda8bda1a4627fd42a8a03450f1
MahenDeCreator <125333089+MahenDeCreator@users.noreply.github.com>
2023-08-23 15:21:37 +0530

    Initial commit

---
+++ quiz-js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was Apple founded?",
+    answer: "1976",
+    options: [
+      "2001",
+      "1981",
+      "1976",
+      "1985"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is a device which contains a speaker and a microphone collectively ?",
+    answer: "headset",
+    options: [
+      "speaker",
+      "microphone",
+      "soundcard",
+      "headset"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which printer is used to print only characters and symbols and cannot print graphics?",
+    answer: "Daisy Wheel printer",
+    options: [
+      "Daisy Wheel printer",
+      "Ink-Jet printer",
+      "Thermal printer",
+      "Laser printer"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which person is not one of the founders of Apple?",
+    answer: "Mark Zuckerberg",
+    options: [
+      "Steve Jobs",
+      "Mark Zuckerberg",
+      "Steve Wozniak",
+      "Ronald Wayne"
+    ]
+  },
+  {
+    numb: 5,
+    question: "In a display specified as 600 x 400 the number of pixels across the display screen is______.",
+    answer: "240000",
+    options: [
+      "600",
+      "400",
+      "240000",
+      "None of the above"
+    ]
+  },
+   {
+     numb: 6,
+     question: "What was Apple's first product?",
+    answer: "Apple I",
+     options: [
+       "Macintosh",
+       "iPod",
+       "Apple I",
+       "Apple watch"
+     ]
+   },
+   {
+     numb: 7,
+     question: "Physical structure of a computer is called:",
+     answer: "Hardware",
+     options: [
+       "Software",
+       "Hardware",
+       "Human ware",
+       "None of the above"
+     ]
+   },
+   {
+     numb: 8,
+     question: "PARAM is an example of:",
+     answer: "Supercomputer",
+     options: [
+       "Supercomputer",
+       "PC",
+       "Laptop",
+       "PDA"
+     ]
+   },
+   {
+     numb: 9,
+     question: "In the world today, most computers are:",
+     answer: "Digital",
+     options: [
+       "Digital",
+       "Hybrid",
+       "Analog",
+       "Complex"
+     ]
+   },
+   {
+     numb: 10,
+     question: "Who is the CEO of Apple?",
+     answer: "Tim Cook",
+     options: [
+       "Jeff Williams",
+       "Tim Cook",
+       "Luca Maestri",
+      "Arvind Krishna"
+     ]
+   },
+
+];
